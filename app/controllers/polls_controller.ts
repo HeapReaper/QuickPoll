720889624bad2eacf14440464cf250dbb4d5a576
AeroBytesNL <kelvin@aerobytes.nl>
2025-05-22 09:52:50 +0200

    Working vote system

--- app/controllers/polls_controller.ts
+++ app/controllers/polls_controller.ts
@@ -4,7 +4,36 @@ import Vote from '#models/vote'
 
 export default class PollsController {
   async index({ view }: HttpContext) {
-    return view.render('pages/create')
+    const latestPollsRaw = await Poll.query()
+      .orderBy('created_at', 'desc')
+      .limit(5)
+      .preload('options', (query) => {
+        query.preload('vote')
+      })
+
+    const latestPolls = latestPollsRaw.map(poll => {
+      const totalVotes = poll.options.reduce((sum, option) => sum + (option.vote?.count ?? 0), 0)
+
+      const optionsWithPercentage = poll.options.map(option => {
+        const count = option.vote?.count ?? 0
+        const percentage = totalVotes > 0 ? Math.round((count / totalVotes) * 100) : 0
+
+        return {
+          id: option.id,
+          name: option.name,
+          count,
+          percentage,
+        }
+      })
+
+      return {
+        id: poll.id,
+        name: poll.name,
+        options: optionsWithPercentage,
+      }
+    })
+
+    return view.render('pages/create', { polls: latestPolls })
   }
 
   async store({ request, response, session }: HttpContext) {
@@ -21,7 +50,7 @@ export default class PollsController {
     return response.redirect(`/poll/${poll.id}`)
   }
 
-  async show({ params, view }: HttpContext) {
+  async show({ params, view, session }: HttpContext) {
     const poll: Poll = await Poll.query()
       .where('id', params.id)
       .preload('options', (query) => {
@@ -43,6 +72,7 @@ export default class PollsController {
       }
     })
 
+    session.flash('success', 'Poll created successfully!')
     return view.render('pages/show', {
       poll: {
         id: poll.id,
@@ -50,5 +80,36 @@ export default class PollsController {
         options: optionsWithPercentage,
       },
     })
+  }
+
+  async vote({ params, session, response, request }: HttpContext) {
+    const { pollId, optionId } = params
+
+    const newVote: Vote = await Vote.findByOrFail('id', optionId)
+    const previousOptionId = request.cookie(`voted_poll_${pollId}`)
+
+    if (previousOptionId) {
+      if (Number.parseInt(previousOptionId) === Number.parseInt(optionId)) {
+        session.flash('error', 'You already voted for this option.')
+        return response.redirect().back()
+      }
+
+      const previousVote = await Vote.find(previousOptionId)
+      if (previousVote) {
+        previousVote.count = Math.max(0, previousVote.count - 1)
+        await previousVote.save()
+      }
+    }
+
+    newVote.count += 1
+    await newVote.save()
+
+    response.cookie(`voted_poll_${pollId}`, optionId, {
+      httpOnly: true,
+      maxAge: '7d',
+    })
+
+    session.flash('success', 'Your vote has been updated!')
+    return response.redirect().back()
   }
 }
