2af1ec64082883a59ec71b1d2998f0d91549f247
AeroBytesNL <kelvin@aerobytes.nl>
2025-05-22 14:21:06 +0200

    Put websocket listener in async function

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,39 +5,42 @@ export const transmit = new Transmit({
   baseUrl: window.location.origin,
 })
 
-const subscription = transmit.subscription('poll-updated')
-await subscription.create()
+async function initSubscription() {
+  const subscription = transmit.subscription('poll-updated')
+  await subscription.create()
 
-subscription.onMessage((message) => {
-  console.log(`Poll updated`)
+  subscription.onMessage((message) => {
+    console.log(`Poll updated`)
 
-  const pollSection = document.querySelector('section[data-poll-id="' + message.pollId + '"]')
-  if (!pollSection) return
+    const pollSection = document.querySelector('section[data-poll-id="' + message.pollId + '"]')
+    if (!pollSection) return
 
-  message.options.forEach(option => {
-    const form = pollSection.querySelector(`form[data-option-id="${option.id}"]`)
-    if (!form) return
+    message.options.forEach(option => {
+      const form = pollSection.querySelector(`form[data-option-id="${option.id}"]`)
+      if (!form) return
 
-    const optionEl = form.querySelector('button > div')
-    if (!optionEl) return
+      const optionEl = form.querySelector('button > div')
+      if (!optionEl) return
 
-    const barEl = optionEl.querySelector('.poll-bar')
-    if (barEl) {
-      if (option.percentage > 0) {
-        barEl.style.width = `${option.percentage}%`
-        barEl.style.display = 'block'
-      } else {
-        barEl.style.display = 'none'
+      const barEl = optionEl.querySelector('.poll-bar')
+      if (barEl) {
+        if (option.percentage > 0) {
+          barEl.style.width = `${option.percentage}%`
+          barEl.style.display = 'block'
+        } else {
+          barEl.style.display = 'none'
+        }
       }
-    }
 
-    const textEl = optionEl.querySelector('.poll-label')
-    if (textEl) {
-      textEl.textContent = `${option.name} (${option.percentage}%)`
-    }
+      const textEl = optionEl.querySelector('.poll-label')
+      if (textEl) {
+        textEl.textContent = `${option.name} (${option.percentage}%)`
+      }
+    })
   })
+}
 
-})
+void initSubscription()
 
 window.Alpine = Alpine
 Alpine.start()
