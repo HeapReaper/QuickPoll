a64e6ea91c94a55afefa6035d815a3373a6597dd
AeroBytesNL <kelvin@aerobytes.nl>
2025-05-23 23:26:22 +0200

    Moved lot of logic to service

--- start/routes.ts
+++ start/routes.ts
@@ -2,15 +2,11 @@ import router from '@adonisjs/core/services/router'
 const PollsController = () => import('#controllers/polls_controller')
 import transmit from '@adonisjs/transmit/services/main'
 
-router.get('/test-broadcast', async () => {
-  transmit.broadcast('poll-updated', { poll: 1 })
-  return { success: true }
-})
-
 router.get('/', [PollsController, 'index'])
 
 router.post('/polls-store', [PollsController, 'store'])
 router.get('/poll/:id', [PollsController, 'show'])
 router.post('/polls/:pollId/vote/:optionId', [PollsController, 'vote'])
 router.post('/polls/delete/:id', [PollsController, 'delete'])
+
 transmit.registerRoutes()
