f8bcf7f4c44c06665a586b99916147dfdad51076
AeroBytesNL <kelvin@aerobytes.nl>
2025-05-24 21:16:47 +0200

    Removed unused import

--- start/routes.ts
+++ start/routes.ts
@@ -1,13 +1,13 @@
 import router from '@adonisjs/core/services/router'
 const PollsController = () => import('#controllers/polls_controller')
 import transmit from '@adonisjs/transmit/services/main'
-import { throttle, throttlePollCreation } from '#start/limiter'
+import { throttlePollCreation } from '#start/limiter'
 
 router.get('/', [PollsController, 'index'])
 
 router.post('/polls-store', [PollsController, 'store']).use(throttlePollCreation)
 router.get('/poll/:id', [PollsController, 'show'])
-router.post('/polls/:pollId/vote/:optionId', [PollsController, 'vote'])//.use(throttle)
+router.post('/polls/:pollId/vote/:optionId', [PollsController, 'vote']) //.use(throttle)
 router.post('/polls/delete/:id', [PollsController, 'delete']).use(throttlePollCreation)
 
 transmit.registerRoutes()
