4ae7049571785fa87ddf88fc2ce2c95cb01c4392
AeroBytesNL <kelvin@aerobytes.nl>
2025-05-24 20:42:16 +0200

    Bugfix

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,12 @@ export default defineConfig({
        * Entrypoints of your application. Each entrypoint will
        * result in a separate bundle.
        */
-      entrypoints: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/transmit.js'],
+      entrypoints: [
+        'resources/css/app.css',
+        'resources/js/app.js',
+        'resources/js/transmit.js',
+        'resources/js/infiniteScroll.js',
+      ],
 
       /**
        * Paths to watch and reload the browser on file change
